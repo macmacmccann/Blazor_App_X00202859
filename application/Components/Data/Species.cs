c1a98c2988463f6a7021ad671b26e039dbadbc23
Mark McCann <markmccann98@outlook.com>
2025-11-25 20:19:26 +0000

    Species Search and Listing

--- application/Components/Data/Species.cs
+++ application/Components/Data/Species.cs
@@ -5,6 +5,10 @@ public class Species
     public required int key { get; set; }
     public required string scientificName { get; set; }
     public required string canonicalName { get; set; }
+    //JsonPropertyName("vernacularName")] uncomment if its dif 
+    public string? vernacularName { get; set; }  // laymans name used for search 
+
+
 
 }
 
--- application/obj/Debug/net8.0/application.AssemblyInfo.cs
+++ application/obj/Debug/net8.0/application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7764c11b3c14fbbfd39ca09213a9a121ab54692")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc1b13441a5bc3c1a9551d1889ed516dc7883e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
